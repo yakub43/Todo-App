33a20acadff40e9a1832031c4fc99a1be1dda6dc
yakub43 <yakubakku43@gmail.com>
2024-03-11 20:59:44 +0530

    modified index.js file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // with express.json() middleware
 
 const app = require(express);
-const {createPayload} = require("./types")
+const {createTodo, updateTodo} = require("./types")
 
 app.use(express.json());
 
@@ -15,6 +15,13 @@ app.use(express.json());
 
 app.post("/todo", function(req, res){
     const createPayload = req.body;
+    const parsePayload = createPayload.safeParse(createPayload);
+    if (!parsePayload.success){
+        res.status(411).json({
+            msg:"Wrong Inputs",
+        })
+        return;
+    }
 
 })
 
@@ -23,6 +30,13 @@ app.get("/todos", function(req, res){
 })
 
 app.put("/completed", function(req, res){
-
+    const updatePaload = req.body;
+    const parsePayload = updateTodo.safeParse(updatePaload);
+    if (!parsePayload.success)(
+        res.status(411).json({
+            msg:"You sent the wrong inputs",
+        })
+    )
+    return;
 })
 
